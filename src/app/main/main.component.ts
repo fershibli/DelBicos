ee8a66318f63560f52f25aee967dfbdb00adec8b
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-06 22:09:22 -0300

    feat(main): Adicionar componente de navbar
    Adiciona o componente de navbar ao componente principal (main) da aplicação.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NavbarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
