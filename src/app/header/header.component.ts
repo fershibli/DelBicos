5807f34b0c10734c4c4dcd1bbbc058befc907b9b
FerShibli <fernandoluiz.1991@gmail.com>
2024-10-17 15:36:19 -0300

    refactor(header): Update header component styles and functionality
    - Update header component CSS to remove unused styles and add new styles for logo and user menu
    - Update header component HTML to include new logo image and user menu functionality
    - Update header component TypeScript to add toggleMenu() function and event listeners for menu closing
    
    Closes #123

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,48 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  menulogin=false;
+  titulosite = 'Extensões Fatec';
+  menuusuario = false;
+  isLoggedIn = false;
+  userName = 'José';
+  isBrowser: boolean;
+
+  constructor() {
+    this.isBrowser =
+      typeof window !== 'undefined' && typeof document !== 'undefined';
+  }
+
+  ngOnInit() {
+    if (this.isBrowser) {
+      document.addEventListener('click', this.onDocumentClick.bind(this));
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.isBrowser) {
+      document.removeEventListener('click', this.onDocumentClick.bind(this));
+    }
+  }
+
+  toggleMenu() {
+    this.menuusuario = !this.menuusuario;
+  }
+
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const menuElement = document.querySelector('.menu-usuario');
+    const userIconElement = document.querySelector('.icone-usuario');
+
+    if (
+      menuElement &&
+      !menuElement.contains(target) &&
+      userIconElement &&
+      !userIconElement.contains(target)
+    ) {
+      this.menuusuario = false;
+    }
+  }
 }
