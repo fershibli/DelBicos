9b78ae56166bfaeb9fcec697060057d02c3f0fb4
FerShibli <fernandoluiz.1991@gmail.com>
2024-10-17 16:56:55 -0300

    feat(main): Add carousel component to main page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent],
+  imports: [HeaderComponent, NavbarComponent, CarouselComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrls: ['./main.component.css'],
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
