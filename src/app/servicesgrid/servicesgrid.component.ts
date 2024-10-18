f15ac4148f4358ca60da126e4472a3a90a6cc9a7
FerShibli <fernandoluiz.1991@gmail.com>
2024-10-17 21:10:26 -0300

    refactor(servicesgrid): Update services grid component
    - Update ngFor loop in servicesgrid.component.html to include index as i
    - Add modal functionality to servicesgrid.component.html and servicesgrid.component.ts
    - Add bootstrap modal library to servicesgrid.component.ts
    - Add openModal method to servicesgrid.component.ts to handle modal opening
    - Update servicesgrid.component.ts to include selectedItem property for modal functionality
    - Update servicesgrid.component.css to include styles for modal
    
    Refactor the services grid component to include a modal functionality. This includes updating the ngFor loop in the HTML template to include the index as i, adding the necessary HTML and TypeScript code for the modal, and including the required styles in the CSS file.

--- src/app/servicesgrid/servicesgrid.component.ts
+++ src/app/servicesgrid/servicesgrid.component.ts
@@ -1,16 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
-const listaImagens = ['BelezaCosmetica.jpg', 'BemEstarQualidadeVida.jpg', 'ReformasReparos.jpg', 'ServicosDomesticos.jpg', 'ServicosPets.jpg']
+const listaImagens = ['BelezaCosmetica.jpg', 'BemEstarQualidadeVida.jpg', 'ReformasReparos.jpg', 'ServicosDomesticos.jpg', 'ServicosPets.jpg'];
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-servicesgrid',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './servicesgrid.component.html',
-  styleUrl: './servicesgrid.component.css',
+  styleUrls: ['./servicesgrid.component.css'],
 })
 export class ServicesgridComponent {
+  selectedItem: number | null = null;
+
+  openModal(item: number) {
+    try {
+    console.log('openModal chamado com índice:', item);
+    this.selectedItem = item;
+    const modalElement = document.getElementById('itemModal');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  } catch (error) {
+    console.error('Erro ao abrir modal:', error);
+  }
+  }
+
   itensVitrine = Array.from({ length: 30 }, (_, i) => {
     const randomPos = Math.floor(Math.random() * listaImagens.length);
     return {
