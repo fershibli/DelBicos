b30e130b046d4b2c97d49fe83e79135eb976f92f
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2024-11-21 13:43:52 -0300

    Componente Header-Logout
    Header padrão para usuários antes do login

---
+++ header-logout/header-logout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-logout.component.html',
+  styleUrl: './header-logout.component.css'
+})
+export class HeaderLogoutComponent {
+
+}
