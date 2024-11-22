19e465c4a1c74c2d0e1956e3319b3220b549165d
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2024-11-22 15:17:47 -0300

    Componente Resultado
    Exibição de uma box com o resultado de profissionais junto ao componente de filtros

--- src/app/Agendamento/resultado/resultado.component.ts
+++ src/app/Agendamento/resultado/resultado.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderLogonComponent } from "../../header-logon/header-logon.component";
+import { BoxFiltrosComponent } from "../box-filtros/box-filtros.component";
+import { BoxProfissionaisComponent } from "../box-profissionais/box-profissionais.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-resultado',
   standalone: true,
-  imports: [],
+  imports: [HeaderLogonComponent, BoxFiltrosComponent, BoxProfissionaisComponent, FooterComponent],
   templateUrl: './resultado.component.html',
   styleUrl: './resultado.component.css'
 })
