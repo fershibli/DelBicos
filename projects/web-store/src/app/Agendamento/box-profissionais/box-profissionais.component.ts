5f210a6bd9e240ce448944f9fca2667d4fd1199b
douglaswenzel <douglas.wenzel@hotmail.com>
2024-11-27 11:41:22 -0300

    Inserção dos dados no componente box-profissionais

--- projects/web-store/src/app/Agendamento/box-profissionais/box-profissionais.component.ts
+++ projects/web-store/src/app/Agendamento/box-profissionais/box-profissionais.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BoxHorariosDisponiveisComponent } from '../box-horarios-disponiveis/box-horarios-disponiveis.component';
 
 @Component({
   selector: 'app-box-profissionais',
   standalone: true,
-  imports: [],
+  imports: [BoxHorariosDisponiveisComponent],
   templateUrl: './box-profissionais.component.html',
   styleUrl: './box-profissionais.component.css'
 })
 export class BoxProfissionaisComponent {
-
+  @Input() profissional = {
+    nome: 'Jefferson Santos',
+    avaliacaoText: 'Muito bom',
+    nota: 4.4,
+    numeroAvaliacoes: 12,
+    foto: 'https://i.pinimg.com/736x/b1/aa/73/b1aa73786a14bf19fb208dfbf90488e5.jpg',
+    categoria: 'Cabeleireiro, Barbeiro',
+    endereco: 'Rua Com. Hermelino Matarazzo, 298',  
+    servico: 'Corte, Barba, Degradê, Social, Degradê navalhado',
+    precoMin: 25.00
+  };
 }
--- projects/web-store/src/app/cadastro/cadastro.component.ts
+++ projects/web-store/src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,14 +9,34 @@ import { CommonModule } from '@angular/common';
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
-  selectedOption: string | null = null;
+export class CadastroComponent  {
+  cadastroForm: FormGroup;
+  selectedOption: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(2)]],
+      sobrenome: ['', [Validators.required, Validators.minLength(2)]],
+      cpf: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
+      genero: ['', Validators.required],
+      dataNascimento: ['', Validators.required],
+      celular: ['', [Validators.required, Validators.pattern(/^\d{10,11}$/)]],
+      localizacao: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      termos: [false, Validators.requiredTrue],
+    });
+  }
 
   selectOption(option: string) {
-    if (this.selectedOption === option) {
-      this.selectedOption = null;  
-    } else { 
-      this.selectedOption = option;  
+    this.selectedOption = option;
+  }
+
+  onSubmit() {
+    if (this.cadastroForm.valid) {
+      console.log('Formulário válido:', this.cadastroForm.value);
+    } else {
+      console.log('Formulário inválido:', this.cadastroForm.controls);
     }
   }
 }
