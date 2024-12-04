1b96dc6c1055dd95b23468fd08234a813a9239c8
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 13:36:09 -0300

    Add ChamadoComponent with HTML, CSS, and unit tests

---
+++ projects/admin/src/app/Administrador/analise/chamado/chamado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado',
+  standalone: true,
+  imports: [],
+  templateUrl: './chamado.component.html',
+  styleUrl: './chamado.component.css'
+})
+export class ChamadoComponent {
+
+}
