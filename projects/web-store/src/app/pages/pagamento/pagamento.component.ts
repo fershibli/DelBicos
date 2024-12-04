471f45c691c60eb615e3520091fb4cf86a67258d
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 16:58:50 -0300

    Remove header logon and footer components from pagamento page layout

--- projects/web-store/src/app/pages/pagamento/pagamento.component.ts
+++ projects/web-store/src/app/pages/pagamento/pagamento.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderLogonComponent } from '../../header-logon/header-logon.component';
 import { FooterComponent } from '../../footer/footer.component';
 import { BlueBarComponent } from '../../blue-bar/blue-bar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pagamento',
   standalone: true,
-  imports: [ CommonModule, HeaderLogonComponent, FooterComponent, BlueBarComponent],
+  imports: [
+    CommonModule,
+    HeaderLogonComponent,
+    FooterComponent,
+    BlueBarComponent,
+    RouterModule,
+  ],
   templateUrl: './pagamento.component.html',
-  styleUrl: './pagamento.component.css'
+  styleUrl: './pagamento.component.css',
 })
 export class PagamentoComponent {
   servico = {
@@ -17,8 +24,8 @@ export class PagamentoComponent {
     id: 12475,
     status: 'Aguardando pagamento',
     preco: 50.0,
-    qrcode: 'https://pngimg.com/uploads/qr_code/qr_code_PNG10.png', 
-    codigoPagamento: '00020126540014br.gov.bcb.pi'
+    qrcode: 'https://pngimg.com/uploads/qr_code/qr_code_PNG10.png',
+    codigoPagamento: '00020126540014br.gov.bcb.pi',
   };
 
   copiarCodigo() {
