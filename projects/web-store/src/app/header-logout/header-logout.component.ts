75aeb32102b27f92563af332904fe18add26674f
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 16:56:56 -0300

    Add header logout component with styling and routing links

---
+++ projects/web-store/src/app/header-logout/header-logout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header-logout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './header-logout.component.html',
+  styleUrl: './header-logout.component.css',
+})
+export class HeaderLogoutComponent {}
