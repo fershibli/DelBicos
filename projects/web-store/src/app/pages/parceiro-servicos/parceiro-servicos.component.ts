c2143b6b2da53da3ae42c14b63e54d36976c1ded
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 16:58:37 -0300

    Remove header logon and footer components from parceiro-servicos page layout

--- projects/web-store/src/app/pages/parceiro-servicos/parceiro-servicos.component.ts
+++ projects/web-store/src/app/pages/parceiro-servicos/parceiro-servicos.component.ts
@@ -3,25 +3,32 @@ import { HeaderLogonComponent } from '../../header-logon/header-logon.component'
 import { FooterComponent } from '../../footer/footer.component';
 import { ParceiroComponent } from '../../parceiro/parceiro/parceiro.component';
 import { ServicosComponent } from '../../parceiro/servicos/servicos.component';
-import { MenuComponent } from "../../parceiro/menu/menu.component";
+import { MenuComponent } from '../../parceiro/menu/menu.component';
 import { BlueBarComponent } from '../../blue-bar/blue-bar.component';
 
 @Component({
   selector: 'app-parceiro-servicos',
   standalone: true,
-  imports: [HeaderLogonComponent, FooterComponent, ParceiroComponent, ServicosComponent, MenuComponent, BlueBarComponent],
+  imports: [
+    HeaderLogonComponent,
+    FooterComponent,
+    ParceiroComponent,
+    ServicosComponent,
+    MenuComponent,
+    BlueBarComponent,
+  ],
   templateUrl: './parceiro-servicos.component.html',
-  styleUrl: './parceiro-servicos.component.css'
+  styleUrl: './parceiro-servicos.component.css',
 })
 export class ParceiroServicosComponent {
   parceiroData = {
     nome: 'Solange Hammer',
-    fotoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRET_cZIlzK0d8ig_gfQEsx4wkBNf4M1IqIIA&s',
+    fotoUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRET_cZIlzK0d8ig_gfQEsx4wkBNf4M1IqIIA&s',
     categoria: 'Beleza e estética',
     avaliacao: 4.3,
     numeroAvaliacoes: 120,
     endereco: 'Av. Barão de Tatuí, 320 - Jardim Vergueiro, Sorocaba - SP',
-    tags: ['Atende em seu endereço', 'Requer agendamento' ]
+    tags: ['Atende em seu endereço', 'Requer agendamento'],
   };
-
 }
