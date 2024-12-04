76308e95084f70fe6f57381c6e2a345b1d17ed26
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 14:45:27 -0300

    Add HomeComponent route to main application routing

--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,35 +1,34 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './Administrador/dashboard/dashboard.component';
 import { ClientesComponent } from './Administrador/gerenciamento/clientes/clientes.component';
 import { ProfissionaisComponent } from './Administrador/gerenciamento/profissionais/profissionais.component';
 import { ServicosComponent } from './Administrador/gerenciamento/servicos/servicos.component';
 import { AtendimentoComponent } from './Administrador/analise/atendimento/atendimento.component';
 import { ChamadoComponent } from './Administrador/analise/chamado/chamado.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: DashboardComponent
-    },
-    {
-        path: 'administrador/gerenciamento/clientes',
-        component: ClientesComponent
-    },
-    {
-        path: 'administrador/gerenciamento/profissionais',
-        component: ProfissionaisComponent
-    },
-    {
-        path: 'administrador/gerenciamento/servicos',
-        component: ServicosComponent
-    },
-    {
-        path: 'administrador/analise/atendimento',
-        component: AtendimentoComponent
-    },
-    {
-        path: 'administrador/analise/chamado',
-        component: ChamadoComponent
-    }
-
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'administrador/gerenciamento/clientes',
+    component: ClientesComponent,
+  },
+  {
+    path: 'administrador/gerenciamento/profissionais',
+    component: ProfissionaisComponent,
+  },
+  {
+    path: 'administrador/gerenciamento/servicos',
+    component: ServicosComponent,
+  },
+  {
+    path: 'administrador/analise/atendimento',
+    component: AtendimentoComponent,
+  },
+  {
+    path: 'administrador/analise/chamado',
+    component: ChamadoComponent,
+  },
 ];
