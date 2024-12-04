01b5ec0d4207c61947a2151666ef55619b922c9d
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 16:57:30 -0300

    Replace header logout component with carousel in home page layout

--- projects/web-store/src/app/pages/home/home.component.ts
+++ projects/web-store/src/app/pages/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { BoxPesquisaComponent } from '../../box-pesquisa/box-pesquisa.component';
-import { HeaderLogoutComponent } from "../../header-logout/header-logout.component";
 import { CategoriasComponent } from '../../categorias/categorias.component';
-import { FooterComponent } from "../../footer/footer.component";
+import { FooterComponent } from '../../footer/footer.component';
+import { CarouselComponent } from '../../carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BoxPesquisaComponent, HeaderLogoutComponent, CategoriasComponent, FooterComponent],
+  imports: [
+    BoxPesquisaComponent,
+    CategoriasComponent,
+    FooterComponent,
+    CarouselComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
