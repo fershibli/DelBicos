ab8ba311c792e72b0e632a033f5552bf3cd25578
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 16:59:31 -0300

    Update menu links and add routing to services component

--- projects/web-store/src/app/parceiro/servicos/servicos.component.ts
+++ projects/web-store/src/app/parceiro/servicos/servicos.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ParceiroComponent } from '../parceiro/parceiro.component';
-import { MenuComponent } from "../menu/menu.component";
+import { MenuComponent } from '../menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-servicos',
   standalone: true,
-  imports: [CommonModule, ParceiroComponent, MenuComponent],
+  imports: [CommonModule, ParceiroComponent, MenuComponent, RouterModule],
   templateUrl: './servicos.component.html',
-  styleUrl: './servicos.component.css'
+  styleUrl: './servicos.component.css',
 })
 export class ServicosComponent {
   produtos = [
@@ -16,15 +17,15 @@ export class ServicosComponent {
       foto: 'https://nucleocursos.com.br/blog/wp-content/uploads/2022/08/Curso-de-Alongamento-de-Cilios-em-Blumenau-Nucleo-Cursos-1024x657-1-1.jpg',
       titulo: 'Aplicação de Cílios fio-a-fio',
       key: 'Aplicação',
-      preco: 50.00,
-      status: 'Horários disponíveis'
+      preco: 50.0,
+      status: 'Horários disponíveis',
     },
     {
       foto: 'https://estudioshine.com.br/wp-content/uploads/2024/03/Volume-brasileiro-1.png',
       titulo: 'Volume Brasileiro',
       key: 'Aplicação',
-      preco: 36.00,
-      status: 'Horários disponíveis'
+      preco: 36.0,
+      status: 'Horários disponíveis',
     },
     // {
     //   foto: 'https://gmcilios.com.br/wp-content/uploads/2024/03/DESIGN-DE-SOBRANCELHA-2.jpg',
@@ -41,5 +42,4 @@ export class ServicosComponent {
     //   status: 'Horários disponíveis'
     // }
   ];
-  ;
 }
