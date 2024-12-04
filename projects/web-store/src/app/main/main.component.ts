04662770cd0448acaf8e34a09308910a82b92646
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 16:56:45 -0300

    Implement conditional header rendering based on user authentication status

--- projects/web-store/src/app/main/main.component.ts
+++ projects/web-store/src/app/main/main.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { CarouselComponent } from '../carousel/carousel.component';
-import { ServicesgridComponent } from '../servicesgrid/servicesgrid.component';
-import { FooterComponent } from "../footer/footer.component";
-import { MeusAgendamentosComponent } from "../pages/meus-agendamentos/meus-agendamentos.component";
+import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { HeaderLogonComponent } from '../header-logon/header-logon.component';
+import { HeaderLogoutComponent } from '../header-logout/header-logout.component';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../serviços/auth.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    HeaderComponent,
-    NavbarComponent,
-    CarouselComponent,
-    ServicesgridComponent,
+    CommonModule,
     FooterComponent,
-    MeusAgendamentosComponent
-],
+    RouterModule,
+    HeaderLogonComponent,
+    HeaderLogoutComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
-export class MainComponent {}
+export class MainComponent implements OnInit {
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.isLoggedIn.subscribe(
+      (status: boolean) => (this.isLoggedIn = status)
+    );
+  }
+}
