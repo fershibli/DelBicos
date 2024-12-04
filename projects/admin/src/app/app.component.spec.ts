64e14a9923ac91416406b2b948a2c1b9f19669d6
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 13:34:41 -0300

    Add MainComponent with header, footer, and routing configuration

--- projects/admin/src/app/app.component.spec.ts
+++ projects/admin/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Admin' title`, () => {
+  it(`should have the 'DelBicos' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Admin');
+    expect(app.title).toEqual('DelBicos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Admin');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DelBicos');
   });
 });
--- projects/admin/src/app/app.routes.ts
+++ projects/admin/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Administrador/dashboard/dashboard.component';
+import { ClientesComponent } from './Administrador/gerenciamento/clientes/clientes.component';
+import { ProfissionaisComponent } from './Administrador/gerenciamento/profissionais/profissionais.component';
+import { ServicosComponent } from './Administrador/gerenciamento/servicos/servicos.component';
+import { AtendimentoComponent } from './Administrador/analise/atendimento/atendimento.component';
+import { ChamadoComponent } from './Administrador/analise/chamado/chamado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    },
+    {
+        path: 'administrador/gerenciamento/clientes',
+        component: ClientesComponent
+    },
+    {
+        path: 'administrador/gerenciamento/profissionais',
+        component: ProfissionaisComponent
+    },
+    {
+        path: 'administrador/gerenciamento/servicos',
+        component: ServicosComponent
+    },
+    {
+        path: 'administrador/analise/atendimento',
+        component: AtendimentoComponent
+    },
+    {
+        path: 'administrador/analise/chamado',
+        component: ChamadoComponent
+    }
+
+];
