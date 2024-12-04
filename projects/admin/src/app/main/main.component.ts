a8a68743eba46e32df48bb8864c6a1fe010d4952
FerShibli <fernandoluiz.1991@gmail.com>
2024-12-04 14:44:33 -0300

    Add NavBarComponent to main layout for improved navigation

--- projects/admin/src/app/main/main.component.ts
+++ projects/admin/src/app/main/main.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from "../footer/footer.component";
+import { FooterComponent } from '../footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    FooterComponent,
-    RouterModule
-],
+  imports: [HeaderComponent, FooterComponent, RouterModule, NavBarComponent],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
